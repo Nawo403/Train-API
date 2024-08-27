2b8a21e3b1c708c994b8ff42f95461d0b7bbe0fd
Thilini Gunawardhana <126796156+Nawo403@users.noreply.github.com>
2024-08-27 21:30:23 +0530

    MongoDB setup

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const trainAPI = express();
 
-const TRAIN_API_PORT = 8081;
+const TRAIN_API_PORT = 8080;
+const MONGO_URI = process.env.MONGO_URI;
 
 trainAPI.use(cors());
 trainAPI.use(express.json());
 
+// MongoDB Connection
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Routes
 trainAPI.get("/", (req, res) => {
   res.status(200).json({ message: "Train API Test" });
 });
 
+// Start server
 trainAPI.listen(TRAIN_API_PORT, () => {
-  console.log(`Weather API is running on port ${TRAIN_API_PORT}`);
+  console.log(`Train API is running on port ${TRAIN_API_PORT}`);
 });
